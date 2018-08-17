74b7c7b018054a3df2d464ecd1b134b44fc5b6b7
ryansrogers <ryansrogers85@gmail.com>
2018-08-17 18:50:16 -0400

    Added devise and omniauth with google auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  
+  resources :tests
+  root 'tests#index'
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
