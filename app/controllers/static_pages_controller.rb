aa59335176a76c90b78aed1856c8007e002a3190
ryansrogers <ryansrogers85@gmail.com>
2018-08-18 16:57:31 -0400

    adding public static pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+
+	skip_before_action :authenticate_user!
+
+	def home
+	end
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   
   resources :tests
-  root 'tests#index'
+  root 'static_pages#home'
+
+
 
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", registrations: "registrations" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
